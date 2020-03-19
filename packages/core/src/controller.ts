3809a9416cfd24d67e5db284dd4544490a518de1
Ben Kelly <b-kelly@users.noreply.github.com>
2020-03-18 23:31:46 -0400

    documentation

--- packages/core/src/controller.ts
+++ packages/core/src/controller.ts
@@ -1,39 +1,78 @@
 export class Controller {
+    /**
+     * A map of all the bindings attached to a controller's targets, keyed on the target name
+     */
     static bindings: BindingMap = {};
+
+    /**
+     * A map of all the helper functions this controller exposes, keyed on the exposed helper's name
+     */
     static helpers: HelperMap = {};
     
-    baseElement: HTMLElement;
+    /**
+     * The base element this controller is attached to
+     */
+    readonly baseElement: HTMLElement;
 
-    protected boundMethods: {[key: string]: EventListener } = { };
+    /**
+     * Map of all events this controller has dynamically bound to the document via bindDocumentEvent
+     */
+    private boundDocumentMethods: {[key: string]: EventListener } = { };
 
     constructor(el: HTMLElement) {
         this.baseElement = el;
     }
 
+    /**
+     * Lifecyle method that is called immediately after the controller instance has been constructed
+     */
     construct() {
         // Override in extending class
     }
 
+    /**
+     * Lifecycle method that is called after a controller has been completely connected
+     */
     connect() {
         // Override in extending class
     }
 
+    /**
+     * Lifecycle method that is called after a controller has been disconnected
+     */
     disconnect() {
         // Override in extending class
     }
 
+    /**
+     * The data allocated to this controller via data-* attributes on the base element
+     */
     protected get data() {
         return this.baseElement.dataset;
     }
 
+    /**
+     * Returns the first element for the given target
+     * @param target The target to fetch
+     */
     protected target(target: string) {
         return this.baseElement.querySelector<HTMLElement>(`[ov-target="${target}"]`);
     }
 
+    /**
+     * Returns all elements for the given target
+     * @param target The targets to fetch
+     */
     protected targets(target: string) {
         return this.baseElement.querySelectorAll<HTMLElement>(`[ov-target="${target}"]`);
     }
 
+    /**
+     * Triggers an event on this controller's baseElement with the event name prefixed with the controller's simple name
+     * @param eventName The name of the event to fire which will be prefixed as `controller-name:eventName`
+     * @param detail The event detail object to attach to the custom event
+     * @param optionalElement An element to fire the event on rather than the baseElement
+     */
     protected triggerEvent<T>(eventName: string, detail?: T, optionalElement?: Element) {
         const namespacedName = Controller._getDomName(this['constructor'].name) + ':' + eventName;
         var event : CustomEvent<T> = new CustomEvent(namespacedName, {bubbles: true, cancelable: true, detail: detail});
@@ -41,46 +80,74 @@ export class Controller {
         return event;
     }
 
+    /**
+     * Cleanly binds an event to the document in a way that can be easily unbound later
+     * @param eventType The type of event to bind
+     * @param key The unique key to reference this event for unbinding later
+     * @param listener The event listener for this event
+     */
     protected bindDocumentEvent(eventType: string, key: string, listener: EventListener) {
-        let boundFunction = this.boundMethods[key] || listener.bind(this);
+        let boundFunction = this.boundDocumentMethods[key] || listener.bind(this);
 
-        this.boundMethods[key] = boundFunction;
+        this.boundDocumentMethods[key] = boundFunction;
 
         document.addEventListener(eventType, boundFunction);
     }
 
+    /**
+     * Unbinds a previously bound document event
+     * @param eventType The type of event to unbind
+     * @param key The unique key used when binding this event originally
+     */
     protected unbindDocumentEvent(eventType: string, key: string) {
-        let boundFunction = this.boundMethods[key];
+        let boundFunction = this.boundDocumentMethods[key];
 
         if (!boundFunction) {
             return;
         }
 
         document.removeEventListener(eventType, boundFunction);
-        delete this.boundMethods[key];
+        delete this.boundDocumentMethods[key];
     }
 
     /**
-     * Translates this controller name to the name you'd use to reference it in the dom
+     * Translates this controller's name to the name you'd use to reference it in the dom
      * eg "TestSampleController" becomes "test-controller"
      */
     public static getDomName() {
         return this._getDomName(this.name);
     }
 
+    /**
+     * Translates a controller name string to the name you'd use to reference it in the dom
+     * eg "TestSampleController" becomes "test-controller" 
+     * @param name 
+     */
     private static _getDomName(name: string) {
         return name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase().replace(/-?controller$/, '').trim();
     }
 }
 
+/**
+ * Describes the shape of a controller helper function
+ */
 export type Helper = (instance: Controller, data?: any) => any;
 
+/**
+ * Describes the shape of a controller binding value
+ */
 export type Binding = [keyof HTMLElementEventMap, EventListener];
 
+/**
+ * Describes the shape of all the helper functions on a controller
+ */
 export interface HelperMap {
-    [key: string]: Helper
+    [helperName: string]: Helper
 }
 
+/**
+ * Describes the shape of all bindings on a controller
+ */
 export interface BindingMap {
-    [key: string]: Binding
+    [targetName: string]: Binding
 }
