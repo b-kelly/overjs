5348e75601233f7810e5e81b62b84ad6fc111639
Ben Kelly <b-kelly@users.noreply.github.com>
2020-03-18 23:39:26 -0400

    documentation

--- packages/core/src/observer.ts
+++ packages/core/src/observer.ts
@@ -3,13 +3,22 @@ export class Observer {
     private observee: Element;
     private delegates: ObserverDelegates;
 
+    /**
+     * Creates a new Observer on the given element
+     * @param observee The element to watch for changes
+     * @param delegates The delegate functions to call when a change happens
+     */
     constructor(observee: Element, delegates: ObserverDelegates) {
         this.delegates = delegates;
         this.observee = observee;
         
+        // create a new mutation observer and have it call `mutate` when a change happens
         this.internalObserver = new MutationObserver(this.mutate.bind(this));        
     }
 
+    /**
+     * Instructs the observer to start listening to the observee for changes
+     */
     connect() {
         this.internalObserver.observe(this.observee, {
             attributes: true,
@@ -18,14 +27,25 @@ export class Observer {
         });
     }
 
+    /**
+     * Instructs the observer to stop watching the observee for changes
+     */
     disconnect() {
         this.internalObserver?.disconnect();
     }
 
+    /**
+     * Gathers all applicable elements and reports them as "added" to the delegates.additionDelegate
+     */
     refresh() {
         this.handleAddedElements(this.delegates.getApplicableElements(this.observee));
     }
 
+    /**
+     * Handles all dom mutations, sorting by type and ensuring the right delegates get called
+     * @param mutations All mutation records reported by the MutationObserver
+     * @param observer The MutationObserver that reported the mutations
+     */
     private mutate(mutations: MutationRecord[], observer: MutationObserver) {
         mutations.forEach(mutation => {
             if (mutation.type === 'childList') {
@@ -39,25 +59,45 @@ export class Observer {
         });
     }
 
+    /**
+     * Fires the additionDelegate for all added elements
+     * @param nodes The nodes that were added in the mutation
+     */
     private handleAddedElements(nodes: NodeList | Node[]) {
+        //TODO filter on applicable elements only?
         nodes.forEach((n: HTMLElement) => {
             this.delegates.additionDelegate(n);
         });
     }
 
+    /**
+     * Fires the removalDelegate for all removed elements
+     * @param nodes The nodes that were removed in the mutation
+     */
     private handleRemovedElements(nodes: NodeList | Node[]) {
+        //TODO filter on applicable elements only?
         nodes.forEach((n: HTMLElement) => {
             this.delegates.removalDelegate(n);
         });
     }
 
+    /**
+     * TODO implement
+     * @param node 
+     */
     private handleAttributeChange(node: Node) {
         // TODO
     }
 }
 
+/**
+ * Describes the shape of a MutationDelegate callback
+ */
 export type MutationDelegate = (element: HTMLElement) => void;
 
+/**
+ * Describes the collection of delegates callbacks needed by an Observer 
+ */
 export interface ObserverDelegates {
     additionDelegate: MutationDelegate;
     removalDelegate: MutationDelegate;
